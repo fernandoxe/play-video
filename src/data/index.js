667525249e5bd1ea448ffb82bbcd8c6112cb9870
Fernando Parra <fernandoxe@gmail.com>
2021-09-26 01:35:12 -0300

    chore: example data

--- src/data/index.js
+++ src/data/index.js
@@ -49,3 +49,20 @@ export const series = [
     ],
   },
 ];
+
+export const movies = [
+  {
+    name: 'Black.Widow.2021',
+    description: 'Black Widow 2021',
+    size: '2.2GB',
+    duration: '02:13:55',
+    endingDuration: '0',
+  },
+  {
+    name: 'Evangelion.1.0.You.Are.Not.Alone.2007',
+    description: 'Evangelion 1.0 You Are (Not) Alone 2007',
+    size: '1.92GB',
+    duration: '01:40:57',
+    endingDuration: '0',
+  },
+];
