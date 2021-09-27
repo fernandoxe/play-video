d1a0d1aded475180d1a17f82e751b527b9a83def
Fernando Parra <fernandoxe@gmail.com>
2021-09-27 02:18:50 -0300

    chore: react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Home } from './pages/Home';
 import { GlobalStyle } from './styles';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Season } from './pages/Season';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/:series">
+          <Season />
+        </Route>
+      </BrowserRouter>
     </>
   );
 }
