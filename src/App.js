f03cd877b903e7ee688da43573497231bef3da3c
Fernando Parra <fernandoxe@gmail.com>
2021-09-28 01:28:25 -0300

    chore: added switch component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Home } from './pages/Home';
 import { GlobalStyle } from './styles';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Season } from './pages/Season';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Series } from './pages/Series';
+import { Player } from './pages/Player';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/:series">
-          <Season />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/series/:series">
+            <Series />
+          </Route>
+          <Route exact path="/series/:series/:season/:episode">
+            <Player />
+          </Route>
+          <Route exact path="/movie/:movie">
+            <Player />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </>
   );
