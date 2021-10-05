c0dd6b091c9c52c5e9021d988d56befde3a891f3
Fernando Parra <fernandoxe@gmail.com>
2021-10-05 00:59:22 -0300

    chore: video tag

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  .video {
+    width: 100%;
+  }
 `;
 
 export const Video = (props) => {
@@ -8,6 +11,11 @@ export const Video = (props) => {
   return (
     <Container>
       <h1>Video {url}</h1>
+      <video className="video">
+        <source src={`/media/${url}.mp4`} type="video/mp4" />
+        <track src={`/media/${url}_es.vtt`} kind="subtitles" srcLang="es"/>
+        <track src={`/media/${url}_en.vtt`} kind="subtitles" srcLang="en"/>
+      </video>
     </Container>
   );
 };
