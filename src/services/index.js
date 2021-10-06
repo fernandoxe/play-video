6cc41f538f2a70fcc3bfb8eb4e4210cc8ec8e07e
Fernando Parra <fernandoxe@gmail.com>
2021-10-06 01:02:15 -0300

    chore: get movie and episode services

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+import { movies, series } from '../data';
+
+export const getMovie = (movieName) => {
+  const movie = movies.find(movie => movie.name === movieName);
+  return movie;
+};
+
+export const getEpisode = (seriesName, seasonNumber, episodeNumber) => {
+  console.log('series', series);
+  const season = series.find(s => s.name === seriesName && s.season === Number(seasonNumber));
+  const episode = season.episodes[episodeNumber - 1];
+  return episode;
+};
