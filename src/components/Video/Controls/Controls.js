96630d0d69cd2e0e0f588c65d01ccc0163c117a3
Fernando Parra <fernandoxe@gmail.com>
2021-10-29 01:34:45 -0300

    chore: controls component

---
+++ src/components/Video/Controls/Controls.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+`;
+
+export const Controls = props => {
+  return (
+    <Container>
+      Controls
+    </Container>
+  );
+};
