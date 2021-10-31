7ccf48a2079542bb7c30b9b281f1ac3537e10b3f
Fernando Parra <fernandoxe@gmail.com>
2021-10-31 01:17:42 -0300

    chore: add listeners

--- src/components/Video/Controls/index.js
+++ src/components/Video/Controls/index.js
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -5,6 +7,7 @@ const Container = styled.div`
 
 export const Controls = props => {
   // const {} = props;
+  const rangeRef = useRef(null);
 
   const handleChange = (event) => {
     console.log('change', event.target.value);
@@ -14,12 +17,23 @@ export const Controls = props => {
     console.log('input', event.target.value);
   };
 
+  useEffect(() => {
+    console.log('add listeners');
+    const inputRange = rangeRef.current; // copy current ref to use the same in remove listeners
+    inputRange.addEventListener('change', handleChange);
+    inputRange.addEventListener('input', handleInput);
+    return () => {
+      console.log('remove listeners');
+      inputRange.removeEventListener('change', handleChange);
+      inputRange.removeEventListener('input', handleInput);
+    };
+  }, []);
+
   return (
     <Container>
       <input
         type="range"
-        onChange={handleChange}
-        onInput={handleInput}
+        ref={rangeRef}
       />
     </Container>
   );
