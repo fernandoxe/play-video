fcbcce62c8c24c7348b7f9bb97c767d280864cc9
Fernando Parra <fernandoxe@gmail.com>
2021-11-03 01:59:16 -0300

    chore: removed unused

--- src/components/Video/Controls/index.js
+++ src/components/Video/Controls/index.js
@@ -3,84 +3,15 @@ import { useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  position: relative;
-
-  .range {
-    width: 100%;
-    appearance: none;
-    background-color: transparent;
-    margin: 0;
-
-    ::-webkit-slider-thumb {
-      -webkit-appearance: none;
-      width: 16px;
-      height: 16px;
-      border-radius: 50%;
-      background-color: gray;
-      cursor: pointer;
-    }
-
-    ::-moz-range-thumb {
-      appearance: none;
-      width: 16px;
-      height: 16px;
-      border-radius: 50%;
-      background-color: gray;
-      cursor: pointer;
-    }
-  }
-
-  .range, .rail, .progress {
-    position: absolute;
-    height: 4px;
-  }
-
-  .rail {
-    width: 100%;
-    background-color: lightgray;
-  }
-
-  .progress {
-    background-color: purple;
-  }
+  
 `;
 
 export const Controls = props => {
   // const {} = props;
-  const rangeRef = useRef(null);
-  const [progress, setProgress] = useState(0);
-
-  const handleChange = (event) => {
-    console.log('change', event.target.value);
-  };
-
-  const handleInput = (event) => {
-    setProgress(event.target.value);
-    console.log('input', event.target.value);
-  };
-
-  useEffect(() => {
-    console.log('add listeners');
-    const inputRange = rangeRef.current; // copy current ref to use the same in remove listeners
-    inputRange.addEventListener('change', handleChange);
-    inputRange.addEventListener('input', handleInput);
-    return () => {
-      console.log('remove listeners');
-      inputRange.removeEventListener('change', handleChange);
-      inputRange.removeEventListener('input', handleInput);
-    };
-  }, []);
 
   return (
     <Container>
-      <div className="rail"></div>
-      <div className="progress" style={{width: `${progress}%`}}></div>
-      <input
-        type="range"
-        className="range"
-        ref={rangeRef}
-        value={progress}
-      />
+      
     </Container>
   );
 };
