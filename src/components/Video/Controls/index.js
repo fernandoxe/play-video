b33b4648d7a11f09c9618274b3abbc52b9f96076
Fernando Parra <fernandoxe@gmail.com>
2021-11-04 01:42:14 -0300

    chore: added progress component

--- src/components/Video/Controls/index.js
+++ src/components/Video/Controls/index.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
 import { useEffect } from 'react';
 import styled from 'styled-components';
+import { Progress } from './Progress';
 
 const Container = styled.div`
   
@@ -9,9 +10,22 @@ const Container = styled.div`
 export const Controls = props => {
   // const {} = props;
 
+  const handleInput = value => {
+    console.log('input', value);
+  };
+
+  const handleChange = value => {
+    console.log('change', value);
+  };
+
   return (
     <Container>
-      
+      <Progress
+        min={0}
+        max={1200}
+        onInput={handleInput}
+        onChange={handleChange}
+      />
     </Container>
   );
 };
