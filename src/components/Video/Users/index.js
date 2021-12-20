4df8214c8bae002669d4ab05b88e67b989d2309a
Fernando Parra <fernandoxe@gmail.com>
2021-12-20 01:37:01 -0300

    chore: users list

--- src/components/Video/Users/index.js
+++ src/components/Video/Users/index.js
@@ -1,5 +1,25 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+`;
+
 export const Users = props => {
   return (
-    <div>Users</div>
+    <Container>
+      <ul>
+        <li>
+          Username 0
+        </li>
+        <li>
+          Username 1
+        </li>
+        <li>
+          Username 2
+        </li>
+        <li>
+          Username 3
+        </li>
+      </ul>
+    </Container>
   );
 };
