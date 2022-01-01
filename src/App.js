ca3cd7906d28497a455bfbe063330109be395f06
Fernando Parra <fernandoxe@gmail.com>
2022-01-01 20:08:51 -0300

    chore: disable routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import { Home } from './pages/Home';
 import { GlobalStyle } from './styles';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Series } from './pages/Series';
-import { Player } from './pages/Player';
-import { Header } from './components/Header';
+// import { Home } from './pages/Home';
+// import { Series } from './pages/Series';
+// import { Player } from './pages/Player';
+import { Video } from './pages/Video';
+// import { Header } from './components/Header';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
-        <header>
+        {/* <header>
           <Header />
-        </header>
+        </header> */}
         <main>
           <Switch>
-            <Route exact path="/">
+            {/* <Route exact path="/">
               <Home />
             </Route>
             <Route exact path="/series/:series">
               <Series />
+            </Route> */}
+            <Route exact path="/video/:video">
+              <Video />
             </Route>
-            <Route exact path="/series/:series/:season/:episode">
+            {/* <Route exact path="/series/:series/:season/:episode">
               <Player />
             </Route>
             <Route exact path="/movie/:movie">
               <Player />
-            </Route>
+            </Route> */}
           </Switch>
         </main>
       </BrowserRouter>
