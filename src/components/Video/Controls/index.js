9144065882d90114a0b80e0c3f2ac00fd914f63a
Fernando Parra <fernandoxe@gmail.com>
2022-01-01 20:08:18 -0300

    chore: controls component

--- src/components/Video/Controls/index.js
+++ src/components/Video/Controls/index.js
@@ -1,35 +1,80 @@
-import { useRef, useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Progress } from './Progress';
 import { Play } from './Play';
 import { Timer } from './Timer';
 
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .play {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .bar {
+    padding: 0 8px;
+    background-color: rgba(124, 77, 255, 0.4);
+  }
+
+  .controls {
+    display: flex;
+    align-items: center;
+  }
+
+  .timer {
+    flex-grow: 1;
+  }
+
+  .progress-container {
+    margin-bottom: 4px;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    color: #7C4DFF;
+    padding: 0;
+    margin: 0 8px;
+    display: flex;
+    :first-child {
+      margin-left: 0;
+    }
+    :last-child {
+      margin-right: 0;
+    }
+  }
+
   .connect {
-    color: gray;
+    color: #dddddd;
+    &--connecting {
+      color: #9C27B0;
+    }
     &--connected {
-      color: purple;
+      color: #7C4DFF;
     }
   }
 
   .subtitles {
+    color: #dddddd;
     &--active {
-      
+      color: #7C4DFF;
     }
   }
 `;
 
 export const Controls = props => {
-  const {currentTime, duration} = props;
-
-  const [play, setPlay] = useState(false);
-  const [subtitles, setSubtitles] = useState(false);
+  const [subtitles, setSubtitles] = useState(true);
   const [fullscreen, setFullscreen] = useState(false);
   const [connect, setConnect] = useState(false);
 
-  const handleInput = value => {
-    props.onProgressInput?.(value);
+  const handleChangeUp = value => {
+    props.onProgressChangeUp?.(value);
   };
 
   const handleChange = value => {
@@ -37,48 +82,63 @@ export const Controls = props => {
   };
 
   const handlePlay = value => {
-    setPlay(value);
     props.onPlay?.(value);
   };
 
-  const handleSubtitlesClick = (value) => {
-    setSubtitles(value);
-    props.onSubtitleClick(value);
+  const handleSubtitlesClick = () => {
+    const newSubtitles = !subtitles;
+    setSubtitles(newSubtitles);
+    props.onSubtitlesClick?.(newSubtitles);
   };
 
-  const handleFullscreenClick = (value) => {
-    setFullscreen(value);
-    props.onFullscreenClick?.(value);
+  const handleFullscreenClick = () => {
+    const newFullscreen = !fullscreen;
+    setFullscreen(newFullscreen);
+    props.onFullscreenClick?.(newFullscreen);
   };
 
-  const handleConnectClick = (value) => {
-    setConnect(value);
-    props.onConnectClick?.(value);
+  const handleConnectClick = () => {
+    const newConnect = !connect;
+    setConnect(newConnect);
+    props.onConnectClick?.(newConnect);
   };
 
   return (
     <Container>
-      <Play onClick={handlePlay} />
-      <Timer currentTime={currentTime} duration={duration} />
-      <button className={`connect ${connect ? '--connected': ''}`} onClick={handleConnectClick}>
-        <i className="material-icons">people</i>
-      </button>
-      <button className={`subtitles ${subtitles ? '--active' : ''}`} onClick={handleSubtitlesClick}>
-        <i className="material-icons">subtitles</i>
-      </button>
-      <button className="fullscreen" onClick={handleFullscreenClick}>
-        {fullscreen ?
-          <i className="material-icons">fullscreen</i>
-          :
-          <i className="material-icons">fullscreen_exit</i>
-        }
-      </button>
-      <Progress
-        min={0}
-        max={duration}
-        onInput={handleInput}
-        onChange={handleChange}
-      />
+      <div className="play">
+        <Play
+          playing={props.playing}
+          onClick={handlePlay}
+        />
+      </div>
+      <div className="bar">
+        <div className="controls">
+          <div className="timer">
+            <Timer currentTime={props.currentTime} duration={props.duration} />
+          </div>
+          <button
+            className={`connect ${connect ? 'connect--connecting': ''} ${props.connected ? 'connect--connected' : ''}`}
+            onClick={handleConnectClick}
+          >
+            <i className="material-icons">people</i>
+          </button>
+          <button className={`subtitles ${subtitles ? 'subtitles--active' : ''}`} onClick={handleSubtitlesClick}>
+            <i className="material-icons">subtitles</i>
+          </button>
+          <button className="fullscreen" onClick={handleFullscreenClick}>
+            <i className="material-icons">{fullscreen ? 'fullscreen_exit' : 'fullscreen'}</i>
+          </button>
+        </div>
+        <div className="progress-container">
+          <Progress
+            min={0}
+            max={props.duration}
+            value={props.currentTime}
+            onChange={handleChange}
+            onChangeUp={handleChangeUp}
+          />
+        </div>
+      </div>
     </Container>
   );
 };
