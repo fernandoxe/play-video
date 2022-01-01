5466f5a001a7d781a7091588ec13d68486827207
Fernando Parra <fernandoxe@gmail.com>
2022-01-01 20:06:45 -0300

    chore: progress story

--- src/components/Video/Controls/Progress/index.stories.js
+++ src/components/Video/Controls/Progress/index.stories.js
@@ -9,8 +9,11 @@ export default {
 
 const Template = (args) => <BrowserRouter><Progress {...args} /></BrowserRouter>;
 
+let value = 0;
+
 export const Default = Template.bind({});
 Default.args = {
   min: 0,
   max: 5 * 60 * 1000,
+  value,
 };
